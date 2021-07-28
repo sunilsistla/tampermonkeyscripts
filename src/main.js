22a4c95fbe2aae98fc06600e3d1cec4baf8370e4
Sunil Kumar Sistla <5101097+sunilkumarsistla@users.noreply.github.com>
2021-07-28 13:21:25 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -13,10 +13,13 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
     <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
-    <script type="text/javascript">setTimeout(function() {
-        document.body.style.display = 'block';
-	console.debug('showing contents');
-    }, 1000);</script>
+    <script type="text/javascript">
+    	setTimeout(function() {
+        	document.body.style.display = 'block';
+		console.log('showing contents');
+    	}, 1000);
+	console.log('register');
+    </script>
 </div>
 `;
 
