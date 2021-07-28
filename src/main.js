b0c8a291433d3f7e04e0611e6aece77f00f1ebdf
Sunil Kumar Sistla <5101097+sunilkumarsistla@users.noreply.github.com>
2021-07-28 13:07:56 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 (function onReady() {
+	document.body.style = (document.body.style || {});
+	document.body.style.display = 'none';
 	
 	var META_TAGS = `
     <meta charset="utf-8">
@@ -17,7 +19,6 @@
 </div>
 `;
 
-	Object.assign(document.body.style, {display: 'none'});
 	document.head.insertAdjacentHTML('afterbegin', META_TAGS);
 	document.head.insertAdjacentHTML('beforeend', STYLES);
 	document.body.insertAdjacentHTML('beforeend', SCRIPTS);
