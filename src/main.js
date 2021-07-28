8b92c8e9b3e77548028f447aef951fc29c892e3a
Sunil Kumar Sistla <5101097+sunilkumarsistla@users.noreply.github.com>
2021-07-28 14:04:27 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -15,7 +15,7 @@
 </div>
 `;
 	var SPINNER_HTML = `
-	<div id="main-loader" style="display: flex; justify-content: center; vertical-align: center;">
+	<div id="main-loader" style="display: flex; justify-content: center; vertical-align: center; width: 100vw; height: 100vh;">
 	<svg version="1.1" id="L9" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
   viewBox="0 0 100 100" enable-background="new 0 0 0 0" xml:space="preserve" style="width: 200px; height: 200px;">
     <path d="M73,50c0-12.7-10.3-23-23-23S27,37.3,27,50 M30.9,50c0-10.5,8.5-19.1,19.1-19.1S69.1,39.5,69.1,50">
@@ -43,11 +43,10 @@
 	document.body.insertAdjacentHTML('afterbegin', '<p><br /></p>');
 	
 	const ROOT = document.getElementById('root');
-	ROOT.style.display = 'none';
     	setTimeout(function() {
 		ROOT.style.display = 'block';
 		var loadingEle = document.getElementById('main-loader');
 		loadingEle && loadingEle.remove();
-    	}, 2000);
+    	}, 1000);
 	
 })();
