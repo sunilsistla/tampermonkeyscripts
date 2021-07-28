b0c7d9356ecaf5c4dc05e2d82a87e691f4f31ac2
Sunil Kumar Sistla <5101097+sunilkumarsistla@users.noreply.github.com>
2021-07-28 14:08:48 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -15,9 +15,9 @@
 </div>
 `;
 	var SPINNER_HTML = `
-	<div id="main-loader" style="display: flex; justify-content: center; vertical-align: center; width: 100vw; height: 100vh;">
+	<div id="main-loader" style="display: flex; position:fixed; top: 0; left: 0; width: 100vw; height: 100vh; z-index: 1; background: white;">
 	<svg version="1.1" id="L9" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-  viewBox="0 0 100 100" enable-background="new 0 0 0 0" xml:space="preserve" style="width: 100px; height: 100px;">
+  viewBox="0 0 100 100" enable-background="new 0 0 0 0" xml:space="preserve" style="width: 100px; height: 100px; margin: auto;">
     <path d="M73,50c0-12.7-10.3-23-23-23S27,37.3,27,50 M30.9,50c0-10.5,8.5-19.1,19.1-19.1S69.1,39.5,69.1,50">
       <animateTransform 
          attributeName="transform" 
